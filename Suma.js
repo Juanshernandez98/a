d31aa0cafdbbafe6db5b8557859c1aba0f1f3f2b
Juanh <juanshernandez@unicesar.edu.co>
2021-11-21 01:33:12 -0500

    d

---
+++ Suma.js
@@ -0,0 +1,5 @@
+function sumap(a,b){
+    return parseInt(a)+parseInt(b);
+}
+
+module.exports = sumap;
---
+++ suma.test.js
@@ -0,0 +1,5 @@
+const sumap = require('./suma.js');
+
+test('2 + 5 = 7', () =>{
+    expect(sumap(2,5)).toBe(7);
+});
